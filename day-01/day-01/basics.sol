60faa23e4154f0d7af3b5b2d8360b708b918f2a5
Mulualem <mulualemdarimo76@gmail.com>
2026-01-01 23:37:33 +0100

    day-01

--- day-01/day-01/basics.sol
+++ day-01/day-01/basics.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.34;
 
+pragma solidity ^0.8.0;
 contract Basics{
+    
+    // Declaring state variables
+    uint public var1;
+    uint public var2;
+    uint public sum;
 
+     // Defining public function 
+     // that sets the values of 
+     // the state variables var1 and var2
+ 
+     function set(uint x , uint y)public view returns(uint){
+        var1 = x;
+        var2 = y;
+        sum var1 + var2;
+     }
+
+     // Defining  function to 
+     // print the sum of the state variables
+
+     function get()public view returns(uint){
+        return sum;
+     }
 }
--- day-01/day-01/notes.md
+++ day-01/day-01/notes.md
@@ -2,4 +2,6 @@
 
 ### What is solidity ?
 
-- is an object-oriented and high-level language for implementing Smart contracts
+- is an object-oriented and high-level language for implementing Smart contracts.
+- Solidity is statically typed and supports inheritance, libraries, and complex user-defined types among features.
+- This features enables to create contracts for users such as voting, crowdfunding, blind auctions, and multi-signature wallets.

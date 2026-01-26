b26f59e7d3373dff9387290fca1563c983763ad6
Mulualem <mulualemdarimo76@gmail.com>
2026-01-26 23:26:39 +0100

    day-07

---
+++ day-07/day-07/Operators.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Solidity contract to demonstrate
+// Arithmetic Operator
+
+contract SolidityTest {
+    //Initializing state variables
+
+    uint16 public a = 20;
+    uint16 public b = 10;
+
+    //Initialize a variable with sum
+
+    uint16 public sum = a + b;
+}
